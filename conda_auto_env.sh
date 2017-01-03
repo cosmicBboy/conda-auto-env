b5f71c3c72d1bf6f75b4e893f029e9aed6aab00b
Niels Bantilan <niels@peggedsoftware.com>
2017-01-03 16:35:16 -0500

    simplified the script
    don't try to create environment if it doesn't
    exist

--- conda_auto_env.sh
+++ conda_auto_env.sh
@@ -12,22 +12,15 @@
 #
 
 function conda_auto_env() {
-  if [ -e "environment.yml" ]; then
-    # echo "environment.yml file found"
-    ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
+  if [ -e "conda-env.yml" ]; then
+    ENV=$(head -n 1 conda-env.yml | cut -f2 -d ' ')
     # Check if you are already in the environment
     if [[ $PATH != *$ENV* ]]; then
       # Check if the environment exists
       source activate $ENV
-      if [ $? -eq 0 ]; then
-        :
-      else
-        # Create the environment and activate
-        echo "Conda env '$ENV' doesn't exist."
-        conda env create -q
-        source activate $ENV
-      fi
     fi
+  else
+    source deactivate
   fi
 }
 
